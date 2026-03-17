76059782a6123dc29bd1761ffce3b11b28183bf5
Maciek <ch.maciek@gmail.com>
2026-03-17 15:49:24 +0100

    zadanko 4 lekcja 4

---
+++ testering/zadanko1.py
@@ -0,0 +1,10 @@
+raw_text=" PATIENT_age_YEARS "
+def text_converter(text):
+    new_text_1 = text.strip()
+    new_text_2 = new_text_1.lower()
+    new_text_3 = new_text_2.replace("_"," ")
+    new_text_4 = new_text_3.title()
+
+    new_text_5 = text.strip().lower().replace("_"," ").title()
+    print(new_text_5)
+text_converter(raw_text)
