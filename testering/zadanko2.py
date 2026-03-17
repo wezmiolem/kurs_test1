df051b70becab70c2244e2ee86cf1daf1aa0cc8b
Maciek <ch.maciek@gmail.com>
2026-03-17 16:02:25 +0100

    task 6 lesson 4

---
+++ testering/zadanko2.py
@@ -0,0 +1,10 @@
+Accuracy = 0.9234
+Precision = 0.8891
+Recall = 0.9456
+
+def percentage_converter(number):
+    print(f"{number:.2%}")
+
+percentage_converter(Accuracy)
+percentage_converter(Precision)
+percentage_converter(Recall)
