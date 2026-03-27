292b2d68a0e4eae0c2fc6d8441885be71a7fc548
Maciek <ch.maciek@gmail.com>
2026-03-27 16:15:05 +0100

    lesson7 task 10

---
+++ testering/zadanko12.py
@@ -0,0 +1,18 @@
+import math
+
+list_of_primes = []
+
+for n in range(2, 101):
+    is_prime = True  
+    if n > 2 and n % 2 == 0:
+        is_prime = False
+    elif n > 2:
+        for i in range(3, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                is_prime = False
+                break 
+    
+    if is_prime:
+        list_of_primes.append(n)
+
+print(f"{list_of_primes}, count {len(list_of_primes)}")
